ad938af1d4fd1e80646ca57d6b0a4b9dbcf6dee9
Daniel Landero <dlandero2005@outlook.com>
2026-05-30 21:02:56 -0600

    feat: remove ConfiguracionController, ConfiguracionService, and related DTO and model classes

--- backend/src/main/java/com/sira/repository/ConfiguracionRepository.java
+++
@@ -1,9 +0,0 @@
-package com.sira.repository;
-
-import com.sira.model.Configuracion;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface ConfiguracionRepository extends JpaRepository<Configuracion, String> {
-}
